98b344ae24c65802bba1143a2d44d62dee695455
calilisantos <calilisantos@gmail.com>
2022-07-22 18:57:48 -0300

    feat: populating Deposits seeders

--- backend/src/database/seeders/20220722195836-Deposits.js
+++ backend/src/database/seeders/20220722195836-Deposits.js
@@ -1,24 +1,18 @@
-'use strict';
-
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface) => {
+    await queryInterface.bulkInsert('Deposits',
+      [{
+        client_id: 3,
+        deposit_value: 500,
+      },
+      {
+        client_id: 5,
+        deposit_value: 1000,
+      },
+      ], { timestamps: false });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('Deposits', null, {})
   }
 };
