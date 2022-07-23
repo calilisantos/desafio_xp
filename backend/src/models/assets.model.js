1e6599378b3ec2e003cd0078f84ca0702c53f521
calilisantos <calilisantos@gmail.com>
2022-07-23 00:52:41 -0300

    feat: creating getAllAssets model

--- backend/src/models/assets.model.js
+++ backend/src/models/assets.model.js
@@ -1,11 +1,18 @@
 const { Asset } = require('../database/models')
 
+const getAllAssets = async () => {
+  const assets = await Asset.findAll({
+    attributes: ['ticket', 'shares', 'price'],
+  });
+  return assets;
+}
+
 const getByAsset = async (asset_id) => {
   const asset = await  Asset.findOne({
     attributes: ['ticket', 'shares', 'price'],
     where: {asset_id},
-  })
+  });
   return asset;
 }
 
-module.exports = { getByAsset };
+module.exports = { getAllAssets, getByAsset };
