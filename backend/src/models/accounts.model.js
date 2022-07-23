3d050a742cc07dd85e60da8cc460b16ad5049cc5
calilisantos <calilisantos@gmail.com>
2022-07-23 14:08:23 -0300

    feat: creating newDeposit model

--- backend/src/models/accounts.model.js
+++ backend/src/models/accounts.model.js
@@ -1,4 +1,4 @@
-const { Client } = require('../database/models')
+const { Client, Deposit } = require('../database/models')
 
 const getBalanceById = async (client_id) => {
   const clientBalance = await  Client.findOne({
@@ -8,4 +8,18 @@ const getBalanceById = async (client_id) => {
   return clientBalance;
 };
 
-module.exports = { getBalanceById };
+const newDeposit = async ({client_id, deposit_value}) => {
+  const client = await getBalanceById(client_id);
+  if(!client) return false
+  const deposit = await Deposit.create({ client_id, deposit_value, 
+    attributes: ['client_id', 'deposit_value'],
+  }); Number
+  await Client.update(
+    {balance: Number(client.balance) + Number(deposit_value)},
+    { where: {client_id}},
+  )
+  return `Deposit of $${deposit.deposit_value} made successfully. New balance of ${client.balance} `;
+  };
+
+
+module.exports = { getBalanceById, newDeposit };
