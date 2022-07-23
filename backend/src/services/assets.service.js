fb2f5e64bd326c54fda1f03d499c69fcd2f95d3d
calilisantos <calilisantos@gmail.com>
2022-07-23 00:52:24 -0300

    feat: creating getAllAssets service

--- backend/src/services/assets.service.js
+++ backend/src/services/assets.service.js
@@ -1,9 +1,14 @@
 const assetsModels = require('../models/assets.model');
 
+const getAllAssets = async () => {
+  const assets = await assetsModels.getAllAssets();
+  return assets;
+};
+
 const getByAsset = async (id) => {
   const asset = await assetsModels.getByAsset(id);
   if (!asset) return { status: 404, message: 'Asset does not exist' };
   return asset;
 };
 
-module.exports = { getByAsset };
+module.exports = { getAllAssets, getByAsset };
