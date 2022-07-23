3582bdac623342b55710c50770aa84f5705b014e
calilisantos <calilisantos@gmail.com>
2022-07-23 04:49:30 -0300

    feat: importing accountRouter in routes.js

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const assetsRouter = require('./assets.router');
+const accountRouter = require('./accounts.router');
 
 const router = Router();
 
 router.use('/assets', assetsRouter);
+
+router.use('/account', accountRouter);
 
 module.exports = router;
