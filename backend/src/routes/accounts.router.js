f132da15073088410ad88ef13b8df2725094efef
calilisantos <calilisantos@gmail.com>
2022-07-23 14:45:01 -0300

    feat: importing newWithdraw in accounts.router.js

--- backend/src/routes/accounts.router.js
+++ backend/src/routes/accounts.router.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { getBalanceById, newDeposit } = require('../controllers/accounts.controller');
+const { getBalanceById, newDeposit, newWithdraw } = require('../controllers/accounts.controller');
 
 const router = Router();
 
 router.get('/:id', getBalanceById);
 
 router.post('/deposit', newDeposit);
+
+router.post('/withdraw', newWithdraw);
 
 module.exports = router;
