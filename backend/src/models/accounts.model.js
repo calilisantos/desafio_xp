27a7c14e8fb6427ff625351700ea12806baa856b
calilisantos <calilisantos@gmail.com>
2022-07-24 17:47:45 -0300

    feat: changing return from getBalanceById

--- backend/src/models/accounts.model.js
+++ backend/src/models/accounts.model.js
@@ -1,10 +1,7 @@
 const { Client, Deposit, Withdraw } = require('../database/models')
 
 const getBalanceById = async (client_id) => {
-  const clientBalance = await  Client.findOne({
-    attributes: ['username', 'balance'],
-    where: {client_id},
-  });
+  const clientBalance = await  Client.findOne({ where: {client_id} });
   return clientBalance;
 };
 
