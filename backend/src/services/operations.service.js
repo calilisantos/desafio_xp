307c334e9fab04285244c611674e5e3518ada30f
calilisantos <calilisantos@gmail.com>
2022-07-24 22:25:01 -0300

    feat: creating newAsk operations service

--- backend/src/services/operations.service.js
+++ backend/src/services/operations.service.js
@@ -1,5 +1,16 @@
 const operationsModels = require('../models/operations.model');
 const { getByAsset } = require('../models/assets.model');
+const { custodyByClient } = require('../models/custodies.model');
+
+const newAsk = async (infos) => {
+  const assetInfos = await getByAsset(infos.asset_id);
+  const custodyInfos = await custodyByClient(infos.asset_id, infos.client_id);
+  console.log('custodyInfos=>', custodyInfos);
+  const orderInfo = await operationsModels.newAsk(infos);
+  if (assetInfos.shares === 0) return { status: 422, message: `${assetInfos.ticket} is unavaible` };
+  if (!orderInfo) return { status: 422, message: '"operation_qtd" must be less or equal than the shares in portfolio' };
+  return orderInfo;
+};
 
 const newBid = async (infos) => {
   const assetInfos = await getByAsset(infos.asset_id);
@@ -9,4 +20,4 @@ const newBid = async (infos) => {
   return orderInfo;
 };
 
-module.exports = { newBid };
+module.exports = { newAsk, newBid };
