f52d8f33f41d04b950d1d2c68ca41bba97128870
calilisantos <calilisantos@gmail.com>
2022-07-24 22:24:09 -0300

    feat: importing newAsk in operations.router.js

--- backend/src/routes/operations.router.js
+++ backend/src/routes/operations.router.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
-const { newBid } = require('../controllers/operations.controller');
+const { newAsk, newBid } = require('../controllers/operations.controller');
 
 const router = Router();
 
 router.post('/buy', newBid);
+
+router.post('/sell', newAsk);
 
 module.exports = router;
