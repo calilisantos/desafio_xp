56428cc382d3448b3a6de02df92c78d288f06573
calilisantos <calilisantos@gmail.com>
2022-07-24 22:24:45 -0300

    feat: creating newAsk in operations controller

--- backend/src/controllers/operations.controller.js
+++ backend/src/controllers/operations.controller.js
@@ -1,9 +1,15 @@
 const operationsServices = require('../services/operations.service');
 
+const newAsk = async (req, res) => {
+  const orderInfo = await operationsServices.newAsk(req.body);
+  if (orderInfo.status) return res.status(orderInfo.status).json(orderInfo.message);
+  return res.status(201).json(orderInfo);
+};
+
 const newBid = async (req, res) => {
   const orderInfo = await operationsServices.newBid(req.body);
   if (orderInfo.status) return res.status(orderInfo.status).json(orderInfo.message);
   return res.status(201).json(orderInfo);
 };
 
-module.exports = { newBid };
+module.exports = { newAsk, newBid };
